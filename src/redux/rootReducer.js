5568540ef9c1c4f2c2ea4f4f15b7a18aefb4de34
DmitrovicAleksa <aleksa.dmitrovic@vivifyideas.com>
2021-07-05 15:52:26 +0200

    all changes done

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -1,10 +1,8 @@
 import { combineReducers } from "redux";
-import {registerReducer} from "./user/user.reducer";
-import {loginReducer} from "./user/user.reducer";
+import { authReducer } from "./user/user.reducer";
 
 const rootReducer = combineReducers({
-  registerReducer,
-  loginReducer
+  authReducer
 });
 
 export default rootReducer;
--- src/redux/user/user.reducer.js
+++ src/redux/user/user.reducer.js
@@ -1,48 +1,33 @@
 import { POST_USER_SUCCESS } from "./user.types";
-import { LOGIN_FAILED,LOGIN_SUCCEEDED } from "./user.types";
-
+import { LOGIN_FAILED, LOGIN_SUCCEEDED } from "./user.types";
 
 // REGISTRATION REDUCER
 const intitalState = {
+  token: localStorage.getItem("token") || null,
   user: {},
+  error: false,
 };
 
-export const registerReducer = (state = intitalState, action) => {
+export const authReducer = (state = intitalState, action) => {
   switch (action.type) {
     case POST_USER_SUCCESS:
       return {
         ...state,
         user: action.payload,
       };
-    default:
-      return state;
-  }
-};
-
-
-// LOGIN REDUCER
-const initState = {
-  token: localStorage.getItem('token') || null,
-  user: {},
-  error: false
-}
-
-export const loginReducer = (state = initState, action)=> {
-  switch (action.type) {
     case LOGIN_SUCCEEDED:
       return {
         ...state,
         token: action.payload.access_token,
-        user: action.payload.user
-      }
+        user: action.payload.user,
+      };
     case LOGIN_FAILED:
       return {
         ...state,
         error: true,
-        user: {}
-      }
+        user: {},
+      };
     default:
-      return state
+      return state;
   }
-}
-
+};
